f0013617075fe54d6d171075e8b55c21a869136b
StarMX <5440909@qq.com>
2021-01-18 03:53:28 +0800

    Update hiwifi-c526a-part2.sh

--- ipq-40xx/hiwifi-c526a-part2.sh
+++ ipq-40xx/hiwifi-c526a-part2.sh
@@ -4,6 +4,10 @@ sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_gener
 # 默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
+
+
+sed -i "s/OpenWrt build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
 # 固件架构
 cat >> .config <<EOF
 CONFIG_TARGET_ipq40xx=y
@@ -17,21 +21,21 @@ EOF
 
 # 取消默认启用的包
 cat >> .config <<EOF
-# CONFIG_PACKAGE_luci-app-ddns is not set
+CONFIG_PACKAGE_luci-app-ddns=y
 # CONFIG_PACKAGE_luci-app-adbyby-plus is not set
 # CONFIG_PACKAGE_luci-app-filetransfer is not set
 # CONFIG_PACKAGE_luci-app-vsftpd is not set
-# CONFIG_PACKAGE_luci-app-ssr-plus is not set
+CONFIG_PACKAGE_luci-app-ssr-plus=y
 # CONFIG_PACKAGE_luci-app-unblockmusic is not set
 # CONFIG_PACKAGE_luci-app-arpbind is not set
 # CONFIG_PACKAGE_luci-app-vlmcsd is not set
 # CONFIG_PACKAGE_luci-app-wol is not set
 # CONFIG_PACKAGE_luci-app-ramfree is not set
-# CONFIG_PACKAGE_luci-app-turboacc is not set
+CONFIG_PACKAGE_luci-app-turboacc=y
 # CONFIG_PACKAGE_luci-app-accesscontrol is not set
 # CONFIG_PACKAGE_luci-app-cpufreq is not set
 # CONFIG_PACKAGE_luci-app-ipsec-vpnd is not set
-# CONFIG_PACKAGE_luci-app-zerotier is not set
+CONFIG_PACKAGE_luci-app-zerotier=y
 # CONFIG_PACKAGE_v2ray is not set
 # CONFIG_PACKAGE_shadowsocks-libev-ss-redir is not set
 # CONFIG_PACKAGE_shadowsocksr-libev-server is not set
@@ -52,23 +56,18 @@ CONFIG_PACKAGE_screen=y
 CONFIG_PACKAGE_autocore-arm=y
 EOF
 
-# FFmpeg
-cat >> .config <<EOF
-CONFIG_PACKAGE_libopus=y
-CONFIG_PACKAGE_libx264=y
-CONFIG_PACKAGE_ffmpeg=y
-CONFIG_PACKAGE_ffprobe=y
-EOF
-
 # 常用软件 默认已启用
-#cat >> .config <<EOF
-#CONFIG_PACKAGE_luci=y
-#CONFIG_PACKAGE_luci-app-upnp=y
-#CONFIG_PACKAGE_luci-app-nlbwmon=y
-#CONFIG_PACKAGE_luci-app-samba=y
-#CONFIG_PACKAGE_autosamba=y
-#CONFIG_PACKAGE_default-settings=y
-#EOF
+cat >> .config <<EOF
+CONFIG_PACKAGE_luci=y
+CONFIG_PACKAGE_luci-app-upnp=y
+CONFIG_PACKAGE_luci-app-nlbwmon=y
+CONFIG_PACKAGE_luci-app-samba=y
+CONFIG_PACKAGE_autosamba=y
+CONFIG_PACKAGE_default-settings=y
+CONFIG_PACKAGE_luci-app-mwan3=y
+CONFIG_PACKAGE_luci-i18n-mwan3-zh-cn=y
+CONFIG_PACKAGE_mwan3=y
+EOF
 
 # 其它
 sed -i '21d' package/lean/default-settings/files/zzz-default-settings #禁止网络共享分类到NAS
